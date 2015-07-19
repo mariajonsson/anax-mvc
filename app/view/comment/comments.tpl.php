14a546089459b8fc22ee49e980558ca0dc0a9608
mariajonsson <maria.jonsson@tam-arkiv.se>
2015-07-19 19:14:47 +0200

    Adding editing comments functionality

--- app/view/comment/comments.tpl.php
+++ app/view/comment/comments.tpl.php
@@ -6,7 +6,10 @@
 <div class='comments'>
 <?php foreach ($comments as $id => $comment) : ?>
 <h4>Kommentar #<a href='<?=$this->url->create('comment/edit/'. $id )?>'><?=$id?></a></h4>
-<p><?=dump($comment)?></p>
+<p><?=$comment['name']?>: </p>
+<p><?=$comment['content']?></p>
+<p><?=$comment['timestamp']?></p>
+<p><?=$comment['ip']?></p>
 <?php endforeach; ?>
 </div>
 <?php endif; ?>
