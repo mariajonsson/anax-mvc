718f412ba40e51d841b1dc7aa9c60747a2af2d1e
mariajonsson <maria.jonsson@tam-arkiv.se>
2015-07-26 18:46:34 +0200

    making fields required

--- app/view/comment/edit.tpl.php
+++ app/view/comment/edit.tpl.php
@@ -4,10 +4,11 @@
 <input type=hidden name="redirect" value="<?=$this->url->create($redirect)?>">
 <fieldset>
 <legend>Redigera kommentar (id #<?=$id?>)</legend>
-<p><label>Kommentar:<br/><textarea name='content'><?=$content?></textarea></label></p>
-<p><label>Namn:<br/><input type='text' name='name' value='<?=$name?>'/></label></p>
+<p class='comment'>Fält märkta med * är obligatoriska</p>
+<p><label>Kommentar:*<br/><textarea name='content' required><?=$content?></textarea></label></p>
+<p><label>Namn:*<br/><input type='text' name='name' value='<?=$name?>' required/></label></p>
 <p><label>Hemsida:<br/><input type='text' name='web' value='<?=$web?>'/></label></p>
-<p><label>Epost:<br/><input type='text' name='mail' value='<?=$mail?>'/></label></p>
+<p><label>Epost:*<br/><input type='text' name='mail' value='<?=$mail?>' required/></label></p>
 <p class=buttons>
 <input type='submit' name='doSave' value='Spara' onClick="this.form.action = '<?=$this->url->create('comment/save/'.$pagekey.'/'. $id)?>'"/>
 <input type='reset' value='Återställ'/>
